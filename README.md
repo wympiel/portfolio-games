338631233cf4ddf567d5e674b6f68fc2a34b3b47
wympiel <wymp10@gmail.com>
2020-11-22 22:53:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Quiz games when you answer for question only Yes or No.
+The winner is who get the higher scores result.
---
+++ data.py
@@ -0,0 +1,10 @@
+question_data = [
+    {"text": "Czy HTML jest językiem programowania?", "answer": "True"},
+    {"text": "Czy bit jest większy od bajta?","answer": "False"},
+    {"text": "Czy Windows jest systemem operacyjnym?", "answer": "True"},
+    {"text": "Czy Fedora Core to jedna z dystrybucji Linuksa?", "answer": "True"},
+    {"text": "Czy komputery Apple pracują na systemie operacyjnym Windows?", "answer": "False"},
+    {"text": "Czy safari to przeglądarka internetowa?", "answer": "True"},
+    {"text": "Czy Bash to język powłoki Windows?", "answer": "False" },
+    {"text": "Czy pierwszą nazwą firmy Google była Bacrub", "answer": "True"}
+]
